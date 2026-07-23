cdfec40cdf7874ef8f619d7c7dc217de6f486358
24CDL01-ChristianDuarte <160366029+duartech24@users.noreply.github.com>
2026-07-22 22:22:49 -0600

    Delete Guía 4 Christian Duarte directory

--- Guía 4 Christian Duarte/lecc33_act2_inc1.cs/main.cs
+++
@@ -1,9 +0,0 @@
-Console.Write("Ingrese una frase: ");
-string frase = Console.ReadLine();
-
-char[] caracteres = frase.ToCharArray();
-Array.Reverse(caracteres);
-string fraseInvertida = new string(caracteres);
-
-Console.WriteLine("\n--- RESULTADO ---");
-Console.WriteLine($"Frase invertida: {fraseInvertida}");
